ec2b1de0b0fd145f6f75e90e9640a2bb724b357c
Parjanya Mudunuri <parjanya@gmail.com>
2019-10-22 15:46:23 +0530

    Added in examples for creating roles to read pods and namespaces

--- src/main/java/io/github/parj/controller/ListPodsController.java
+++ src/main/java/io/github/parj/controller/ListPodsController.java
@@ -1,5 +1,13 @@
 package io.github.parj.controller;
 
+import io.fabric8.kubernetes.api.model.Namespace;
+import io.fabric8.kubernetes.api.model.NamespaceList;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import io.github.parj.exception.NotFoundException;
 import io.kubernetes.client.ApiClient;
 import io.kubernetes.client.ApiException;
@@ -7,7 +15,6 @@ import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CoreV1Api;
 import io.kubernetes.client.models.V1Pod;
 import io.kubernetes.client.models.V1PodList;
-import io.kubernetes.client.util.Config;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,25 +26,29 @@ import java.util.HashMap;
 @RequestMapping("/list")
 public class ListPodsController {
 
+    private KubernetesClient getClient() {
+        Config config = new ConfigBuilder().withMasterUrl("https://kubernetes.default.svc").build();
+        return new DefaultKubernetesClient(config);
+    }
+
     @RequestMapping(value = "/pods", method = RequestMethod.GET)
     public HashMap<String, String> listPods() {
         HashMap<String, String> map = new HashMap<>();
-        try {
-            ApiClient client = Config.defaultClient();
-            Configuration.setDefaultApiClient(client);
+        PodList pods = getClient().pods().list();
 
-            CoreV1Api api = new CoreV1Api();
-            V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null);
+        for (Pod pod : pods.getItems())
+            map.put("pod_" + pod.hashCode(), pod.getMetadata().getName());
 
-            for (V1Pod item : list.getItems()) {
-                map.put("pod_" + item.hashCode(), item.getMetadata().getName());
-            }
+        return map;
+    }
 
-            return map;
-        } catch (IOException | ApiException e) {
-            System.err.println("Error while getting pods");
-            e.printStackTrace(System.err);
-            throw new NotFoundException(e.getMessage());
-        }
+    @RequestMapping(value = "/ns", method = RequestMethod.GET)
+    public HashMap<String, String> listNamespaces() {
+        HashMap<String, String> map = new HashMap<>();
+        NamespaceList myNs = getClient().namespaces().list();
+        for (Namespace ns : myNs.getItems())
+            map.put("ns_" + ns.hashCode(), ns.getMetadata().getName());
+
+        return map;
     }
 }
